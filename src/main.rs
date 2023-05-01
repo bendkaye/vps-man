2f47be05a752f7faa9069201760b523991dfc8db
bendkaye <bendkaye@gmail.com>
2023-05-01 15:36:28 +0300

    temp with bug

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use serde::Deserialize;
+use std::fs;
+// use std::io;
+// use std::process::Command;
+
+
+#[derive(Deserialize)]
+struct Alias {
+    name: String,
+    command: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = fs::read_to_string("config.json").expect("Unable to read config file");
+    let aliases: Vec<Alias> = serde_json::from_str(&config).expect("Unable to parse config JSON");
+
+    println!("Available aliases:");
+    for alias in &aliases {
+        println!("- {}", alias.name  );
+
+    }
 }
